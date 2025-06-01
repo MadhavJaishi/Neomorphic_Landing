2e3a217533d0dc3779055644dae5ffcb75f672ab
Madhav Jaishi <madhavjaishi0@gmail.com>
2025-06-01 09:33:19 +0530

    Project Init

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import Footer from "./components/elements/Footer"
+import Navbar from "./components/elements/Navbar"
+
+const App = () => {
+  return (
+    <>
+    <Navbar />
+
+    <main className="flex flex-col gap-y-20 md:gap-y-32 overflow:hidden">
+      
+    </main>
+
+    <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+import react from "@tailwindcss/vite"
+
+export default defineConfig({
+  plugins: [
+    react(), tailwindcss(),
+  ],
+})
